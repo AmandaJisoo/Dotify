86e2603ac590658236f69d7d7c88f669e1424592
AmandaJisoo <amandapark8988@gmail.com>
2021-04-08 17:37:50 -0700

    coloe change compelte

--- app/src/main/java/com/example/dotify1/MainActivity.kt
+++ app/src/main/java/com/example/dotify1/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.example.dotify1
 
 import android.annotation.SuppressLint
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.*
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
 import kotlin.random.Random
 
 
 class MainActivity : AppCompatActivity() {
     var initalNumOfPlay = Random.nextInt(20, 200)
+    @RequiresApi(Build.VERSION_CODES.M)
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +25,7 @@ class MainActivity : AppCompatActivity() {
         setUpPlayBtn()
         setUpPreviousButton()
         setUpNextButton()
+        chageAlbumCoverColor()
     }
 
     @SuppressLint("SetTextI18n")
@@ -48,13 +54,13 @@ class MainActivity : AppCompatActivity() {
 
     @SuppressLint("SetTextI18n")
     fun setUpNumOfPlay() {
-        val songNumber = findViewById<TextView>(R.id.num_of_play)
+        val songNumber = findViewById<TextView>(R.id.numOfPlay)
         songNumber.text = "$initalNumOfPlay plays"
     }
 
     fun setUpPlayBtn() {
         val playButton= findViewById<ImageView>(R.id.playBtn);
-        val numOfPlay = findViewById<TextView>(R.id.num_of_play)
+        val numOfPlay = findViewById<TextView>(R.id.numOfPlay)
         playButton.setOnClickListener() {
             initalNumOfPlay++;
             numOfPlay.text = "$initalNumOfPlay plays";
@@ -72,6 +78,17 @@ class MainActivity : AppCompatActivity() {
         val nextPlatBtn= findViewById<ImageButton>(R.id.nextPlayBtn)
         nextPlatBtn.setOnClickListener() {
             Toast.makeText(applicationContext,"Skipping to next track", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    fun chageAlbumCoverColor() {
+        val albumCover = findViewById<ImageButton>(R.id.albumImage)
+        val numOfPlay = findViewById<TextView>(R.id.numOfPlay)
+        albumCover.setOnLongClickListener() {
+            val rnd = Random.Default
+            numOfPlay.setTextColor(Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256)))
+            return@setOnLongClickListener true
         }
     }
 }
